8d73e110418a26bddce364ad3338d10e0c23c7e1
Daniel Valdivia <dvaldivia98@hotmail.com>
2024-04-08 23:37:02 -0600

    Se creó un Endpoint para poder acceder a los datos de la tabla "roles"

---
+++ src/main/java/com/sistema/notificaciones/controllers/RolController.java
@@ -0,0 +1,29 @@
+package com.sistema.notificaciones.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sistema.notificaciones.models.Rol;
+import com.sistema.notificaciones.services.RolService;
+
+@RestController
+@RequestMapping("/roles")
+@CrossOrigin("*")
+public class RolController {
+    @Autowired
+    private RolService rolService;
+
+    @GetMapping("/")
+    public ResponseEntity<List<Rol>> obtenerRoles(){
+        List<Rol> roles=rolService.obtenerTodosLosRoles();
+        return new ResponseEntity<>(roles, HttpStatus.OK);
+    }
+}
